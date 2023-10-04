d6ef2cf199944b405fa733249a07f7184915bf86
anonymoussubmission9 <146393713+anonymoussubmission9@users.noreply.github.com>
2023-10-03 21:19:07 -0400

    changes

--- job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -J grace-closure-pruned --mem=120GB --account=r_mdnakh --gpus=40gb:1
-#SBATCH --mail-type=BEGIN,END 
-#SBATCH --mail-user=nakhla054@gmail.com 
-
-source /etc/profile.d/modules.sh
-module load anaconda/3.2022.10
-python runtotal.py Cli
