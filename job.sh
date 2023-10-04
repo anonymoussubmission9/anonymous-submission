0c89a0bdb883436a2a8887a04411dde109d86ae5
anonymoussubmission9 <146393713+anonymoussubmission9@users.noreply.github.com>
2023-10-03 21:09:54 -0400

    updated

--- job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -J grace-closure-pruned --mem=120GB --account=r_mdnakh --gpus=1
-#SBATCH --mail-type=BEGIN,END 
-#SBATCH --mail-user=nakhla054@gmail.com 
-
-source /etc/profile.d/modules.sh
-module load anaconda/3.2023.03
-python runtotal.py Jsoup
